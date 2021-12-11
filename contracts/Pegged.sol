1a1253926673150f0eab86d5d53a5b8ccdc1e058
flashfish <flashfish0x@protonmail.com>
2021-12-11 21:02:38 +0000

    pegged initial commit

---
+++ contracts/Pegged.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+contract Pegged is Ownable {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+  
+    
+}
