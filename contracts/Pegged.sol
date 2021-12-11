a2b2e2f6ad4d3aeb810ada0bca27333b5a66ff1f
flashfish <flashfish0x@protonmail.com>
2021-12-11 21:57:41 +0000

    start

--- contracts/Pegged.sol
+++ contracts/Pegged.sol
@@ -8,13 +8,69 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "./PeggedERC20.sol";
 
+struct Option{
+    PeggedERC20 peg;
+    PeggedERC20 nopeg;
+    address oracle;
+    uint32 expiry;
+    uint16 fee; //0-1000 basis points
+
+}
 
 contract Pegged is Ownable {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
-  
+    address public creator; //address that can create insurance
+    address public immutable erc20Implementation;
+    address public treasury;
+    address public collateral;
+    Option[] public options;
+
+    modifier onlyTrusted(){
+        require(msg.sender == owner() || msg.sender == creator, "!Trusted");
+        _;
+    }
+
+    constructor (
+        address _erc20Implementation,
+        address _creator,
+        address _treasury,
+        address _collateral
+        ) public Ownable(){
+        erc20Implementation = _erc20Implementation;
+        creator = _creator;
+        treasury = _treasury;
+        collateral = _collateral;
+    }
+
+    function newOption(address pool, address want, uint32 _expiry, uint16 _fee, address _oracle) public onlyTrusted returns(uint256 id){
+        PeggedERC20 yes = new PeggedERC20("KEEPPEG", "", pool, want);
+        PeggedERC20 no = new PeggedERC20("LOSEPEG", "", pool, want);
+
+        options.push(Option(
+            {
+                peg: yes,
+                nopeg: no,
+                oracle: _oracle,
+                expiry: _expiry,
+                fee: _fee
+            }
+        ));
+        return options.length -1;
+    }
+
+    function mint(uint256 id, uint256 amount) public {
+        IERC20(collateral).safeTransferFrom(msg.sender, address(this), amount);
+        require(id < options.length, "Invalid ID");
+        Option memory op = options[id];
+        op.peg.mint(msg.sender, amount);
+        op.nopeg.mint(msg.sender, amount);
+    }
+
     
+
 }
--- contracts/PeggedERC20.sol
+++ contracts/PeggedERC20.sol
@@ -9,12 +9,22 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PeggedERC20 is ERC20 {
+contract PeggedERC20 is ERC20, Ownable{
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
+    address immutable pool;
+    address immutable want;
 
-    
+    constructor (string memory name, string memory symbol, address _pool, address _want) public ERC20(name, symbol) Ownable(){
+        pool = _pool;
+        want = _want;
+
+    }
+
+    function mint(address _address, uint256 _amount) public onlyOwner{
+        _mint(_address, _amount);
+    }
 
 }
